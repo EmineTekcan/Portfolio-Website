c38eed4da1d421c16d2896ea87bd232acddf56ff
Emine Tekcan <eminetek68can@gmail.com>
2023-05-26 02:03:13 +0300

    Home page completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
+import Admin from '../assets/emine.jpg'
 
 const Home = () => {
     return (
-        <div className='h-screen w-full bg-gradient-to-b from-black to-gray-800 text-white'>
-            <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-6 md:flex-row'> 
-                <div>
-                    <h2>I'm React and React Native Developer</h2>
-                    <p>
+        <div className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
+            <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
+                <div className='flex flex-col justify-center h-3/5 text-white'>
+                    <h2 className='text-4xl font-bold sm:text-6xl'>I'm React and React Native Developer</h2>
+                    <p className='text-gray-500 py-4 max-w-md'>
                         Currently working mostly with React Native to
                         build mobile apps. I am more focused in back-end
                         technologies but also have useful experience with front-end
@@ -16,14 +17,23 @@ const Home = () => {
                         and make life easier.
                     </p>
                     <div>
-                        <button>
+                        <button
+                         className='flex group text-white w-fit px-6 py-3 my-2 rounded-md
+                        items-center hover:scale-105
+                         bg-gradient-to-r from-[#0033cc] to-[#6666ff]
+                         '>
                             Portfolio
-                            <span>
-                                <MdOutlineKeyboardArrowRight />
+                            <span className='ml-3 group-hover:rotate-90 duration-300'>
+                                <MdOutlineKeyboardArrowRight size={30} />
                             </span>
                         </button>
+
                     </div>
                 </div>
+                <div>
+                    <img alt="Admin" src={Admin} className='rounded-2xl mx-auto w-2/3 md:w-full' />
+                </div>
+
             </div>
         </div>
     )
